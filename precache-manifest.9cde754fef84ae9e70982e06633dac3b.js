9533c4eaaf002ca5a4fb55d0543d85f3aeec8c6b
Dillikumar <dillikumar270399@gmail.com>
2020-09-07 12:01:44 +0530

    Updates

---
+++ precache-manifest.9cde754fef84ae9e70982e06633dac3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0619c043c9b36a124761f7d28b494bd3",
+    "url": "/socialFeedApp/index.html"
+  },
+  {
+    "revision": "e32a3014eb3782639d4a",
+    "url": "/socialFeedApp/static/css/main.d4e57616.chunk.css"
+  },
+  {
+    "revision": "4d307d2169774fe4cc8a",
+    "url": "/socialFeedApp/static/js/2.609bfaa8.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/socialFeedApp/static/js/2.609bfaa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32a3014eb3782639d4a",
+    "url": "/socialFeedApp/static/js/main.11416df7.chunk.js"
+  },
+  {
+    "revision": "1711e47e9adfb661af79",
+    "url": "/socialFeedApp/static/js/runtime-main.fdb1914c.js"
+  },
+  {
+    "revision": "42a59a6b5e3548348a0e935e5ad5fc53",
+    "url": "/socialFeedApp/static/media/girl-user.42a59a6b.jpg"
+  },
+  {
+    "revision": "537386abac75390708597abefab87b7b",
+    "url": "/socialFeedApp/static/media/user-1.537386ab.jpg"
+  }
+]);
