29264314f4af4ed448fb4e979acfe699cd312144
Dillikumar <dillikumar270399@gmail.com>
2020-09-07 11:04:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./components/FontawesomeIcons";
+import './App.scss';
+import TopNavigationBar from './components/TopNavigationBar';
+import MainContainer from './components/MainContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       navBarFold:'yes'
+    }
+
+  }
+
+  toFoldNavbar(folded){
+      this.setState({   navBarFold:folded  })
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+      
+          <TopNavigationBar toFoldNavbar={this.toFoldNavbar.bind(this)}/>
+         
+          <MainContainer navBarFold={this.state.navBarFold}/>
+
+      </div>
+    )
+  }
 }
+
 
 export default App;
